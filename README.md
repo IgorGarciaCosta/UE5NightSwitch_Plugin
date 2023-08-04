dd10d82b7977e97c22dfa4f327f1d06bbb410643
ISILV125 <isilv125@ford.com>
2023-08-04 17:17:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NightSwitchPlugin
+
+Developed with Unreal Engine 5
---
+++ Source/NightSwitchPlugin/NightSwitchPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NightSwitchPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTSWITCHPLUGIN_API ANightSwitchPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
